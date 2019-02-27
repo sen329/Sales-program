04c1364a0c9ace448e59a2d973f0142544877a41
sen329 <hans.hilman@gmail.com>
2019-02-27 11:24:09 +0700

    tes commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public user: any = {};
+
+    constructor() { }
+
+  ngOnInit() {
+  }
+
+}
