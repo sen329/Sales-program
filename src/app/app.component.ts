e6a39804b579687127aa8353b03d47b1fedb41b7
sen329 <hans.hilman@gmail.com>
2019-08-13 15:34:11 +0700

    Fixed navbar, win/lose bug, report not showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+
+  
+  get isLoggedIn() {
+    return !!this.authService.isAuthenticated();
+  }
+
+  get name(){
+    return localStorage.getItem('name');
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
 }
+
